3b1e30311f70f85fcf43deb912ce89711dc77952
vikyij <vikkyij54@gmail.com>
2024-11-30 13:10:17 +0100

    implement functionality to use AWS Python SDK(boto3) to retreive data from dynabodb, implement openai to generate movie summary

--- main.py
+++ main.py
@@ -1,39 +1,87 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
-import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+
+client = OpenAI()
+
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Get table
+table = dynamodb.Table('Movie-data')
+
 
 app = FastAPI()
 
-def load_movies(): 
-     with open('data.json', 'r') as file:
-        return json.load(file)
-
 @app.get('/getmovies')
 def getmovies():
-    movies = load_movies()
-    return movies
+    # Get all movies from Dynamodb
+    try:
+        response = table.scan()
+        items = response["Items"]
+        return items
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get('/getmoviesbyyear/{year}')
-def getmoviesbyyear(year: int):
-    movies = load_movies()
-    movie_year = []
-    for movie in movies:
-        if int(movie['releaseYear']) == year:
-            movie_year.append(movie)
-    if movie_year:
-         return movie_year
-    else:
-        raise HTTPException(status_code=404, detail="No movies found for the specified year.")
+def getmoviesbyyear(year: str):
+    # Get movies by year
+    try:
+        response = table.query(
+        IndexName= "releaseYearIndex",
+        KeyConditionExpression=Key('releaseYear').eq(year)
+        )
+        movies = response["Items"]
+        if movies:
+            return movies
+        else:
+            raise HTTPException(status_code=404, detail=f"No movies found for the specified year {year}.")
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get('/getmoviesummary/{movie_title}')
 def getmoviesummary(movie_title: str):
     movie_title = movie_title.replace('_', ' ')
-    movies = load_movies()
-    for movie in movies:
-        if movie['title'].lower() == movie_title.lower():
-            movie['generatedSummary'] = 'A mind-bending sci-fi thriller about dream theft and manipulation.'
-            return movie
-    raise HTTPException(status_code=404, detail="Movie not found.")
+    # STEP 1: Get Items by Title
+    try:
+        response = table.scan(
+            FilterExpression=Attr('title').eq(movie_title.lower())
+        )
+        movies = response["Items"]
+        if not movies:
+            raise HTTPException(status_code=404, detail='Movie not found')
+        movie=movies[0]
+       
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    # STEP 2: Generate the AI Summary
+    try: 
+        prompt = (f"Write a brief and engaging summary for the movie '{movie['title']}' "
+            f"which was released in {movie['releaseYear']} and belongs to the genre {movie['genre']}.")
+ 
+        completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+        return(completion.choices[0].message)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error generating AI summary: {str(e)}")
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
